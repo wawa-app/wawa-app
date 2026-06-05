fdddb0a8aca3cd7a7b70f112e92cdc72c8a103c0
Bella-moonju <mra01@mylangara.ca>
2026-06-04 21:27:56 -0700

    Update client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3000',
-    timeout: 10000, 
+    baseURL: 'http://10.0.2.2:3000',
+    timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
     },
